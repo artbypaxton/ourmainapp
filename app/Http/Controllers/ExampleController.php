c1eaa38959e21cb3017930758c3a943d01f2a8e1
artbypaxton <62125692+artbypaxton@users.noreply.github.com>
2023-04-20 19:22:13 -0500

    Update on Example Controller

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
-    //
+    public function homepage(){
+        return '<h1>Homepage</h1><a href="/about">View the about page</a>'; 
+    }
+
+    public function aboutPage(){
+        return '<h1>About Page</h1><a href="/">Back to home</a>'; 
+    }
 }
